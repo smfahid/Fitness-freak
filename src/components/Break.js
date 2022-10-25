c39e057b34a698b400308aa750ff421329239015
smfahid <esanfahid@gmail.com>
2022-10-25 16:31:12 +0600

    break button done

--- src/components/Break.js
+++ src/components/Break.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './Break.css'
 
 const Break = (props) => {
     return (
         <div className='py-3 w-11/12 m-auto'>
             <p className='text-lg font-bold my-2 mx-3'>Add A Break</p>
             <div className='flex bg-slate-100 rounded-lg'>
-                <button onClick={()=>props.timeHandler(10)} className='mx-2 my-3 bg-white rounded-full p-1 text-sm hover:bg-indigo-700 hover:text-white'>10s</button>
-                <button onClick={()=>props.timeHandler(20)} className='mx-2 my-3 bg-white rounded-full p-1 text-sm hover:bg-indigo-700 hover:text-white'>20s</button>
-                <button onClick={()=>props.timeHandler(30)} className='mx-2 my-3 bg-white rounded-full p-1 text-sm hover:bg-indigo-700 hover:text-white'>30s</button>
-                <button onClick={()=>props.timeHandler(40)} className='mx-2 my-3 bg-white rounded-full p-1 text-sm hover:bg-indigo-700 hover:text-white'>40s</button>
-                <button onClick={()=>props.timeHandler(50)} className='mx-2 my-3 bg-white rounded-full p-1 text-sm hover:bg-indigo-700 hover:text-white'>50s</button>
+                <button onClick={()=>props.timeHandler(10)} className='focus:bg-indigo-700 focus:text-white  mx-2 my-3 bg-white rounded-full p-1 text-sm hover:bg-indigo-700 hover:text-white'>10s</button>
+                <button onClick={()=>props.timeHandler(20)} className='focus:bg-indigo-700 focus:text-white mx-2 my-3 bg-white rounded-full p-1 text-sm hover:bg-indigo-700 hover:text-white'>20s</button>
+                <button onClick={()=>props.timeHandler(30)} className='focus:bg-indigo-700 focus:text-white mx-2 my-3 bg-white rounded-full p-1 text-sm hover:bg-indigo-700 hover:text-white'>30s</button>
+                <button onClick={()=>props.timeHandler(40)} className='focus:bg-indigo-700 focus:text-white mx-2 my-3 bg-white rounded-full p-1 text-sm hover:bg-indigo-700 hover:text-white'>40s</button>
+                <button onClick={()=>props.timeHandler(50)} className='focus:bg-indigo-700 focus:text-white mx-2 my-3 bg-white rounded-full p-1 text-sm hover:bg-indigo-700 hover:text-white'>50s</button>
             </div>
         </div>
     );
